c34636846aa0eeaecb90e7655d24822fb7106117
wxmbugu <>
2024-04-01 09:01:37 +0300

    Fix: Navigation bar
     - Navbar fix with icons and kinda responsive
    Signed-off-by: wxmbugu <>

--- src/dashboard.php
+++ src/dashboard.php
@@ -8,19 +8,19 @@
 <body>
   <header>
     <div class="logo">
-      <a href="#"><img src="../assets/images/logo.png" alt="Logo"></a>
+      <a href="#"><img src="../assets/images/moon.png" alt="Logo"></a>
     </div>
     <div class="title">
-      <h1><a href="#">Title</a></h1>
+      <h1>Fusion</h1>
     </div>
     <nav class="nav-links">
-      <a href="#">Home</a>
-      <a href="#">Exhibits</a>
-      <a href="#">Art</a>
       <a href="#">Galleries</a>
+      <a href="#">Art</a>
+      <a href="#">Exhibits</a>
+      <a href="#">Home</a>
     </nav>
     <div class="cart">
-      <a href="#"><img src="../assets/images/cart-icon.png" alt="Cart"></a>
+      <a href="#"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuaQCz8N8GNnjjeA7ofPcPQY5k42c0UrfRnbRyUFilgA7MiEGGIZ_-1wUwVd_VzJh_ZqQ&usqp=CAU" alt="Cart"></a>
     </div>
     <div class="hamburger-menu">&#9776;</div>
   </header>

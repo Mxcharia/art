9fe5a4305edfa40d526b76f65f92cd92f8802aa2
wxmbugu <>
2024-04-02 12:45:30 +0300

    Cart functionality
     - add to cart functionality in user dashboard
    
    Signed-off-by: wxmbugu <>

--- src/dashboard.php
+++ src/dashboard.php
@@ -5,8 +5,38 @@ if (!isset($_SESSION['user']))
 include '../lib/sql.php';
 
 $db = new Mysql();
+$user_id = $_SESSION['user']['user_id'];
+if (isset($_POST['add_to_cart'])) {
+  $art_id = $_POST['art_id'];
+  // Check if the combination of user_id and art_id already exists in the cart
+  $query = "SELECT * FROM cart WHERE user_id = $user_id AND art_id = $art_id";
+  $cart_item = $db->selectfreerun($query);
+  $cart_item = mysqli_fetch_assoc($cart_item);
+  if ($cart_item) {
+    // If the item already exists, update the quantity
+    $quantity = $cart_item['quantity'] + 1;
+    $db->freerun("UPDATE cart SET quantity = $quantity WHERE user_id = $user_id AND art_id = $art_id");
+  } else {
+    // If the item doesn't exist, insert a new record
+    $db->freerun("INSERT INTO cart (user_id, art_id, quantity) VALUES ($user_id, $art_id, 1)");
+  }
+}
 
-
+if (isset($_POST['add_to_cart_ticket'])) {
+  $exhibit_id = $_POST['exhibit_id'];
+  // Check if the combination of user_id and art_id already exists in the cart
+  $query = "SELECT * FROM cart WHERE user_id = $user_id AND exhibit_id = $exhibit_id";
+  $cart_item = $db->selectfreerun($query);
+  $cart_item = mysqli_fetch_assoc($cart_item);
+  if ($cart_item) {
+    // If the item already exists, update the quantity
+    $quantity = $cart_item['quantity'] + 1;
+    $db->freerun("UPDATE cart SET quantity = $quantity WHERE user_id = $user_id AND exhibit_id = $exhibit_id");
+  } else {
+    // If the item doesn't exist, insert a new record
+    $db->freerun("INSERT INTO cart (user_id, exhibit_id, quantity) VALUES ($user_id, $exhibit_id, 1)");
+  }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -32,7 +62,12 @@ $db = new Mysql();
     <div class="cart">
       <a href="#"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuaQCz8N8GNnjjeA7ofPcPQY5k42c0UrfRnbRyUFilgA7MiEGGIZ_-1wUwVd_VzJh_ZqQ&usqp=CAU" alt="Cart"></a>
       <div class="ccalc">
-        1
+        <?php
+        $user_id = $_SESSION['user']['user_id'];
+        $cart_details = $db->selectwhere('cart', 'user_id', '=', $user_id);
+        $num_rows = mysqli_num_rows($cart_details);
+        echo $num_rows;
+        ?>
       </div>
     </div>
     <div class="hamburger-menu">&#9776;</div>
@@ -61,7 +96,10 @@ $db = new Mysql();
               <p><i><b>Artist name:</b></i> <?php echo $artwork['artist']; ?></p>
               <p><i><b>Price:</b></i> $<?php echo $artwork['price']; ?></p>
             </div>
-            <button class="add-to-cart">&#43; Add to Cart</button>
+            <form method="POST">
+              <input type="hidden" name="art_id" value="<?php echo $artwork['id']; ?>">
+              <button type="submit" class="add-to-cart" name="add_to_cart">&#43; Add to Cart</button>
+            </form>
           </div>
         </div>
     <?php
@@ -91,9 +129,12 @@ $db = new Mysql();
             <h2><?php echo $event['name']; ?></h2>
             <div class="additional-details">
               <p><i><b>Description:</b></i> <?php echo $event['description']; ?></p>
-              <p> <i><b>Price:</b></i>$<?php echo $event['price']; ?></p>
+              <p> <i><b>Price:</b></i> $<?php echo $event['price']; ?></p>
             </div>
-            <button class="add-to-cart">&#43; Buy Ticket</button>
+            <form method="POST">
+              <input type="hidden" name="exhibit_id" value="<?php echo $event['id']; ?>">
+              <button type="submit" class="add-to-cart" name="add_to_cart_ticket">&#43; Add to Cart</button>
+            </form>
           </div>
         </div>
     <?php
